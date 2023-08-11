26576aab621c2c354cbe94ee3053a232d95f1b5b
Shreyas Malvadkar <123150485+ShreyasMalvadkar@users.noreply.github.com>
2023-08-11 21:01:16 +0530

    Update Homepage.jsx

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -8,19 +8,9 @@ import Footer from '../General/Footer'
 import WelcomeModal from '../General/WelcomeModal'
 
 const Homepage = () => {
-    const [show,setShow] =useState(true);
-
     useEffect(()=>{
-        setShow(true);
+        
     },[])
-
-    const handleClose=()=>{
-        setShow(false);
-    }
-
-    setTimeout(function () { 
-        setShow(false); 
-    }, 1200);
 
   return (
     <div onClick={handleClose} className='container-fluid'>
