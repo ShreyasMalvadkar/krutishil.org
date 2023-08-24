978b45f7513ff289022a2f8c8e17f1bf3ea37885
Shreyas Malvadkar <shreyasmalvadkar219@gmail.com>
2023-08-24 20:56:27 +0530

    Add Center Search Func

--- src/App.js
+++ src/App.js
@@ -137,7 +137,10 @@ export const App = () => {
               </Routes>
             </div>
 
-            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 center">
+            <div
+              style={{ paddingTop: "20px" }}
+              className="col-xs-3 col-sm-3 col-md-3 col-lg-3 center"
+            >
               <RightSideMenu />
             </div>
           </div>
--- src/Components/SubCenters/SubCenter.jsx
+++ src/Components/SubCenters/SubCenter.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import NoPravartak from '../Maintainance/NoPravartak';
 import './SubCenter.css'
 import subCentersData from '../General/Subcenters.json'
 import { Button } from '@mui/material';
@@ -14,6 +12,28 @@ function SubCenter() {
   const subCentersReady = [
     'Ahmednagar','Bhandara','Pune','Nashik','Satara','Buldhana','Dhule','Malegaon','Thane','Raigad','Panvel'
   ];
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState();
+    const [searchEnabled, setSearchEnabled]=useState(false);
+  
+    const handleInputChange = (event) => {
+      setSearchEnabled(true);
+      const { value } = event.target;
+      console.log(value)
+      setSearchTerm(value);
+      filterData(value);
+    };
+  
+  
+    const filterData = (searchTerm) => {
+      const filteredData = subCentersData.filter((item) =>
+        item.nameENG.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filteredData);
+    };
+  
 
   
   const navigate= useNavigate();
@@ -38,8 +58,15 @@ function SubCenter() {
     <div className='container-fluid p-2'>
       <div>
         <h2>Sub Center List</h2>
-        <hr/>
-        <p>
+        
+        <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+                 
+        {!searchEnabled && <p>
           {subCentersData.map((data,index)=>{
             return(
               <Button variant="contained" className='p-2 m-2' fontSize='large' onClick={event => handleCityChange(event, data.nameENG)}
@@ -48,7 +75,18 @@ function SubCenter() {
               </Button>
             )
           })}
-        </p>
+        </p>}
+
+        {filteredData && <p>
+          {filteredData.map((data,index)=>{
+            return(
+              <Button variant="contained" className='p-2 m-2' fontSize='large' onClick={event => handleCityChange(event, data.nameENG)}
+              key={index}>
+                <b>{data.nameMR}</b>&nbsp; ({data.nameENG})
+              </Button>
+            )
+          })}
+        </p>}
       </div>
       
         {/* <div>
