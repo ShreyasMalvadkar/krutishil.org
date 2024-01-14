84942f1dc722250dde87b4c0cf986ad8e25ba971
Shreyas Malvadkar <shreyasmalvadkar219@gmail.com>
2024-01-14 11:12:39 +0530

    added: new file in updates

--- src/Components/General/PdfViewer.jsx
+++ src/Components/General/PdfViewer.jsx
@@ -1,32 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
 const PdfViewer = ({ pdfSrc }) => {
   const [loadError, setLoadError] = useState(false);
+
+  pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
   const handleLoadError = () => {
     setLoadError(true);
   };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }}>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden",
+      }}
+    >
       {loadError ? (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+        >
           <p>Failed to load PDF.</p>
         </div>
       ) : (
-        <embed
-          src={pdfSrc}
-          type="application/pdf"
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
-          onError={handleLoadError}
-        />
+        <Document file={pdfSrc} onLoadError={handleLoadError}>
+          <Page pageNumber={1} width="100%" />
+        </Document>
       )}
     </div>
   );
